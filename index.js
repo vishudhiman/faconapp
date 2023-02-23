c86664c664a8aba9e366de4c24b9e821e029ffcf
unknown <vishudhiman1900@gmail.com>
2023-02-23 21:35:12 +0530

    Code refactored

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
 //static files
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')))
 
