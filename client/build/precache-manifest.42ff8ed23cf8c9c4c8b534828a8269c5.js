7c476ee85e419ee08494c609925331667b4790e0
vishudhiman <vishudhiman1900@gmail.com>
2023-04-15 11:04:11 +0530

    New changes added

---
+++ client/build/precache-manifest.42ff8ed23cf8c9c4c8b534828a8269c5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6a66d07daf007879e6695709db8ce5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "decd018f1d5a12311633",
+    "url": "/static/css/main.b8386509.chunk.css"
+  },
+  {
+    "revision": "e01141bcecc0f7a4662e",
+    "url": "/static/js/2.1cd1b16c.chunk.js"
+  },
+  {
+    "revision": "4122d9d6a2d659a742748934e6944b12",
+    "url": "/static/js/2.1cd1b16c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "decd018f1d5a12311633",
+    "url": "/static/js/main.01e99ac5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.01e99ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  },
+  {
+    "revision": "5e10127cbf588df844490b288254769b",
+    "url": "/static/media/404Error.5e10127c.png"
+  },
+  {
+    "revision": "b7d7682a1418ace728674934b99aea72",
+    "url": "/static/media/logo.b7d7682a.png"
+  }
+]);
